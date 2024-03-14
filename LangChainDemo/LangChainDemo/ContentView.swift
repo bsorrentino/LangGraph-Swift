c0bdcb96b32621a724b88efb47020766d649b855
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-03-14 19:42:32 +0100

    feat: setup langchain demo app project

---
+++ LangChainDemo/LangChainDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LangChainDemo
+//
+//  Created by bsorrentino on 14/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LangChainDemo/LangChainDemo/LangChainDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangChainDemoApp.swift
+//  LangChainDemo
+//
+//  Created by bsorrentino on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LangChainDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
